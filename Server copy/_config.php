893e52404740564394a763e6ec049d0592f15ff3
ILY-1606 <no1.ily1606@gmail.com>
2021-04-22 15:57:42 +0700

    Upload

--- Server copy/_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Ho_Chi_Minh');
-$db_username = "u106559149_syncyoutube";
-$db_hostname = "localhost";
-$db_database = "u106559149_syncyoutube";
-$db_password = "Cunguyen9001";
-
-
-?>
--- Server copy/_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once($_SERVER["DOCUMENT_ROOT"]."/_config.php");
-$conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
